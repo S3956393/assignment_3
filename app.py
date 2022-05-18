3e4bd10e4a92c010828a85f1b89f6e8ea26664b1
terezagordon <s3895235@student.rmit.edu.au>
2022-05-18 23:33:23 +1000

    add host to app

--- app.py
+++ app.py
@@ -96,4 +96,4 @@ def get_iso_code():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, port=port, threaded=True)
+    app.run(debug=True, host='0.0.0.0', port=port, threaded=True)
